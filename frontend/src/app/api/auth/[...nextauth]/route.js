635a40b0e59c1dfe5c2c6822ab670bdc79915ec1
Minhaj Ap <minhajap00@gmail.com>
2025-07-01 15:25:02 +0530

    fix: removed export name from authOption to authOptions in NextAuth configuration

--- frontend/src/app/api/auth/[...nextauth]/route.js
+++ frontend/src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOption = {
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -26,5 +26,5 @@ export const authOption = {
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-const handler = NextAuth(authOption);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
